b53cb4076abd9a339ac5814ae7b62c1496e58459
LNNguyen-Bi <nguyen01082004@gmail.com>
2024-09-09 00:45:39 +0700

    bài tập thục hanh 9-9

---
+++ Baitapvenha9-9/Baitapvenha9-9/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Baitapvenha9_9.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Baitapvenha9_9.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Lê Nhật Nguyên";
+            ViewBag.MSSV = "1822040966";
+            ViewData["Nam"] = "Năm 2004";
+            return View();
+        }
+
+
+    }
+
+}
---
+++ Baitapvenha9-9/Baitapvenha9-9/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace baitapvenha9_9.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Lê Nhật Nguyên";
+            ViewBag.MSSV = "1822040966";
+            ViewData["Nam"] = "Năm 2004";
+            return View();
+        }
+    }
+}
