e98ac9c6b00cf46ca9cad0f0ecc1329e2234e95e
LNNguyen-Bi <nguyen01082004@gmail.com>
2024-09-10 15:17:14 +0700

    baikiemtra01

---
+++ BaiTapKiemTra01/Controllers/BaiTap2Controller.cs
@@ -0,0 +1,18 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class BaiTap2Controller : Controller
+    {
+        public IActionResult BaiTap2()
+        {
+            var sanpham = new SanPhamViewModel
+            {
+                TenSp = "coffee chồn",
+                GiaBan = " (20 triệu VNĐ) 1 kg"
+            };
+            return View(sanpham);
+        }
+    }
+}
---
+++ BaiTapKiemTra01/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class TaiKhoanViewModel
+{
+    [Required]
+    [StringLength(100, MinimumLength = 6)]
+    public string Username { get; set; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 6)]
+    public string Password { get; set; }
+
+    public string FullName { get; set; }
+
+    public int Age { get; set; }
+
+    public string PasswordHash { get; set; }
+}
